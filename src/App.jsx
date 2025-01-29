03fccd891c85976c72d606767ab551b5a88a7b57
Nasir <131708693+nasir-siddiqui@users.noreply.github.com>
2025-01-29 16:54:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "../src/components/Header"
 import Category from "./components/Category";
 import Hero from "./components/Hero";
 import DisplayProduct from "./components/DisplayProduct";
+import Footer from "./components/Footer";
 
 
 
@@ -13,13 +14,16 @@ import DisplayProduct from "./components/DisplayProduct";
 function App() {
   return (
     <>
-    <Header />
-    <div className="mx-4">
-      <Category />
-      <Hero />
-      {/* <DisplayProduct /> */}
-    </div>
-    
+      <Header />
+      <div className="flex justify-center mx-4 ">
+        <div className="flex flex-col place-items-center justify-center max-w-full bg-green-100 px-2">
+          <Category />
+          <Hero />
+          <DisplayProduct />
+        </div>
+      </div>
+      <Footer />
+
     </>
   )
 }
